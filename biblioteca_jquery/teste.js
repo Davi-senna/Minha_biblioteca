b82824c71bec4259263d75d0517aae0be387b8fc
Davi ferreira de sena <davifsena2@gmail.com>
2022-04-24 19:17:14 -0300

    Aprendendo ajax

---
+++ biblioteca_jquery/teste.js
@@ -0,0 +1,25 @@
+
+
+// $(document).ready( function() { // Wait until document is fully parsed
+//     $("#teste").on('submit', function(e){
+
+//        e.preventDefault();
+
+//     });
+//   })
+
+$(document).ready(()=>{
+    
+$("#teste").submit((e)=>{
+    e.preventDefault();
+
+    var teste_valor = $("#teste1").val();
+
+    $.ajax({
+        
+    })
+
+});
+});
+
+
---
+++ biblioteca_jquery/teste.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="jquery/jquery.min.js"></script>
+    <script src="teste.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <form id="teste" action="#" method="get">
+        <input required id="teste1" type="text">
+        <h1>fdsfsdfa</h1>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
